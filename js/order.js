63eb22780d55d770604c415c39cdd83cf681f63a
sonomapickleballladder <108966563+sonomapickleballladder@users.noreply.github.com>
2023-01-22 11:35:36 -0800

    Update order.js

--- js/order.js
+++ js/order.js
@@ -167,17 +167,47 @@ async function ptc(t,s,e,m,x,z,thenfunction){
         if(byId('name').value.toLowerCase().trim()==nm.toLowerCase().trim()) {byId('name').value=nm}
         let lzmn = LZString.compressToUTF16(nm);
         //console.log(bnsuawe.indexOf(lzmn)>-1);
-        if(bnsuawe.indexOf(lzmn)>-1 || (byId('name').value.toLowerCase().trim()==nm.toLowerCase().trim() && checkid()[1])) {
-        let p0 = orord.indexOf(nm);
+        //let rel = document.createElement('i');
+       // rel.innerHTML = Array.from(element.parentNode.children).indexOf(element) -  orord.indexOf(nm);
+       // element.after(rel)
+       let p0 = orord.indexOf(nm);
         let vert = Array.from(element.parentNode.children).indexOf(element) - p0;
+        let namespan = element.getElementsByTagName('span')[0];
+
+        if(bnsuawe.indexOf(lzmn)>-1 || (byId('name').value.toLowerCase().trim()==nm.toLowerCase().trim() && checkid()[1])) {
         let indchange =  Math.abs(vert);
         if(vert>0) {indchange = Math.abs(vert+2)}
-        let namespan = element.getElementsByTagName('span')[0];
+        let inp = document.createElement('input');
         //console.log(vert);
         if(vert<0) namespan.style.backgroundColor = clrsup[indchange];
         if(vert>0) namespan.style.backgroundColor = clrsdown[indchange];
         if(vert==0) namespan.style.backgroundColor = "";
+
         }
+
+        let tg = 'small';
+
+        clearel(element, tg)
+
+        let newNode = document.createElement(tg);
+        let invrt = JSON.stringify(-vert);
+        if(parseInt(vert)==0) {invrt = ""};
+        if(invrt.indexOf('-')>-1) {invrt=invrt.replace(/-/g,'')+'⇓'}
+        else if(invrt.length>0) {invrt=invrt+'⇑'}
+        newNode.innerHTML= ` ${invrt} `;
+
+        namespan.parentNode.insertBefore(newNode, namespan.nextSibling);
+
     }
 
+  }
+
+
+
+  function clearel(pr, el) {
+    var list = pr.getElementsByTagName(el);
+    for (var k = list.length - 1; k >= 0; k--) {
+      var item = list[k];
+      item.parentNode.removeChild(item);
+    }
   }
