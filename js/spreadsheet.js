82ba43e62a2ed8d3b3ebab6511fca697b28c3dd1
sonomapickleballladder <108966563+sonomapickleballladder@users.noreply.github.com>
2023-02-13 01:20:03 -0800

    Add files via upload

--- js/spreadsheet.js
+++ js/spreadsheet.js
@@ -15,9 +15,10 @@ function  setrefresh() {
       if(Boolean(byId('link2'))){urls[1].url=byId('link2').getAttribute('url'); urls[2].url=byId('link').getAttribute('url')+dt;}
       let url = urls[2].url, view, alt = '';
       if(ismobile()) {url=urls[1].url}
+      try{if(localStorage.getItem('spreadsheet_view')&&Object.keys(urls).includes(localStorage.getItem('spreadsheet_view'))){url = urls[localStorage.getItem('spreadsheet_view')].url}}catch(ex){}
       if (location.href.indexOf('url=')>-1) {url = urls[parseInt(location.href.split('url=')[1].split('&')[0].split('?')[0])].url;}
       for (let i = 0; i < Object.keys(urls).length; i++) {
-        if(url == Object.values(urls)[i].url) {view = Object.keys(urls)[i];}
+        if(url == Object.values(urls)[i].url) {view = Object.keys(urls)[i]; try{localStorage.setItem('spreadsheet_view',view)}catch(ex){}}
         else {alt = ` <a href='?url=${Object.keys(urls)[i]}'>Click here<a> to load the alternative version.`;}
       }
       try{byId('view').innerHTML=`<br><br>You are viewing a version of this page optimized for ${urls[view].view}.${urls[view].text}${alt}`}catch(ex){}
