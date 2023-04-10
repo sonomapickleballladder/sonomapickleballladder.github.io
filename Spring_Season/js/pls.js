2f2185d49f041d4074f1dc542659c353738d108f
sonomapickleballladder <108966563+sonomapickleballladder@users.noreply.github.com>
2023-04-09 21:41:28 -0700

    Add files via upload

--- Spring_Season/js/pls.js
+++ Spring_Season/js/pls.js
@@ -10,11 +10,12 @@ if(isal) {
         }
     }
 
-    let al = document.createElement('script');
+    /*let al = document.createElement('script');
      al.src = 'https://analytics.projectsegfau.lt/js/script.js';
      al.setAttribute('data-domain', 'sonomapickleballladder.github.io');
      al.setAttribute('defer', '');
-     document.head.appendChild(al)}
+     document.head.appendChild(al)}*/
+    }
 
 
 
