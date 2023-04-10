1c6b49def2dbb6f45e89252de4cc06d106f2631c
sonomapickleballladder <108966563+sonomapickleballladder@users.noreply.github.com>
2023-04-10 01:12:34 -0700

    Add files via upload

--- Spring_Season/js/ladder.js
+++ Spring_Season/js/ladder.js
@@ -1,4 +1,4 @@
 const ladderSeason = '_Spring_2023';
 const thisLadder = 'Open';
-const laddersOffered = {'Open': {ls: '_73605625073234973904_', min: 0, sb: '', url: '/'}, 'Womens': {ls: '_04914861211241326984_', url: '@Womens/'}, 'Mens': {ls: '_41598763447389514591_', url: '@Mens/'}, 'Singles': {ls: '_51600555464256341585_', url: '@Singles/'}, 'Miny-Singles': {ls: '_95391847196677445687_', url: '@Miny-Singles/'}, '2-Week': {ls: '_86749060568300090027_', url: '@2-week/'}}
+const laddersOffered = {'Open': {ls: '_73605625073234973904_', min: 0, sb: '', url: './'}, 'Womens': {ls: '_04914861211241326984_', url: '@Womens/'}, 'Mens': {ls: '_41598763447389514591_', url: '@Mens/'}, 'Singles': {ls: '_51600555464256341585_', url: '@Singles/'}, 'Miny-Singles': {ls: '_95391847196677445687_', url: '@Miny-Singles/'}, '2-Week': {ls: '_86749060568300090027_', url: '@2-week/'}}
 const ladderId = laddersOffered[thisLadder]
