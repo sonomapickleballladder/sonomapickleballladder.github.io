588008ce8240ea26cf206cf54a68c05931b3732f
Pierce Bagjay <info@pb.rocks>
2026-01-17 18:41:09 -0500

    2026 Winter season launch

--- summer_season/js/loader.js
+++
@@ -1,1 +0,0 @@
-function animateLoad(){try{let ld=document.createElement("p");ld.id="loader",ld.style.zIndex=0,ld.innerHTML=`<div class="bigger loader"></div>`;let ifr=document.createElement("iframe");document.body.appendChild(ld)}catch{}}animateLoad()
